2a8fc9d3f52c18ff68fc690b564d3a8d5bfc43ea
Shehan82 <shehansandeepa82@gmail.com>
2020-03-29 09:20:34 +0530

    constructor parameter

--- myclass.java
+++ myclass.java
@@ -1,15 +1,46 @@
 class shehan
 {
-	public shehan()
+	static void print(String word)
 	{
-		System.out.println("Hello i am the constructor");
+		System.out.println(word);
 	}
+
+	static int print(int num)
+	{
+		System.out.println(num);
+		return num;
+	}
+
+	int x;
+	
+	public shehan(int y)
+	{
+		print("Hello i am the constructor");
+		x=y;
+	}
+
+	
+
 }
 
 class myclass
 {
+	static int print(int num)
+	{
+		System.out.println(num);
+		return num;
+	}
+
+	static void print(String word)
+	{
+		System.out.println(word);
+	}
+
 	public static void main(String[] args)
 	{
-		shehan obj=new shehan();
+		shehan obj=new shehan(200);
+		print("hello");
+		print(obj.x);
+
 	}
 }
