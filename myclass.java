d0bba8c3dc1416db369e5098af04b2371fc6affa
Shehan82 <shehansandeepa82@gmail.com>
2020-03-29 08:34:08 +0530

    java simple constructor

---
+++ myclass.java
@@ -0,0 +1,15 @@
+class shehan
+{
+	public shehan()
+	{
+		System.out.println("Hello i am the constructor");
+	}
+}
+
+class myclass
+{
+	public static void main(String[] args)
+	{
+		shehan obj=new shehan();
+	}
+}
