b2c30a0889df74236d6a80836ae51a32c6ef8fca
Shehan82 <shehansandeepa82@gmail.com>
2020-04-26 17:01:40 +0530

    naive string search algo

---
+++ naive.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class naive
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		// int i = Integer.parseInt(myString);
+		String t = scan.nextLine();
+		String p = scan.nextLine();
+
+		int n = t.length();
+		int m = p.length();
+
+		char[] tarray = t.toCharArray();
+		char[] parray = p.toCharArray();
+
+		for (int i=0;i<n-m+1 ;i++ )
+		{
+			int j=0;
+
+			while(j<m && tarray[i+j]==parray[j])
+			{
+				j++;
+			}
+
+			if(j==m)
+			{
+				System.out.println(i);
+			}
+			// else
+			// {
+			// 	System.out.println("there is no substring");
+			// }
+		}
+	}
+}
