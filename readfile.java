80d41e30c9fced1b7182ccccabaa69d15e62b106
shehan82 <shehansandeepa82@.com>
2020-05-21 11:01:36 +0530

    now seraching start with line num 22

--- readfile.java
+++ readfile.java
@@ -31,32 +31,36 @@ class readfile
 				count++;
 
 				String text = read.nextLine();
-				int t_length = text.length();
-				char[] t_array = text.toCharArray();
-
-				for(i=0;i<t_length-p_length+1;i++)
+				if(count>21)
 				{
-					if(t_array[i]==w)
-					{
-						break;
-					}
-					//System.out.print(t_array[i]);
+					int t_length = text.length();
+					char[] t_array = text.toCharArray();
 
-					int j = 0;
-					while(j<p_length && t_array[i+j]==p_array[j])
+					for(i=0;i<t_length-p_length+1;i++)
 					{
-						j++;
-					}
+						if(t_array[i]==w)
+						{
+							break;
+						}
+						//System.out.print(t_array[i]);
 
-					if(j==p_length)
-					{
-						System.out.printf("word occurs in line num : %d and column %d \n",count,i+1);
+						int j = 0;
+						while(j<p_length && t_array[i+j]==p_array[j])
+						{
+							j++;
+						}
+
+						if(j==p_length)
+						{
+							System.out.printf("word occurs in line num : %d and column %d \n",count,i+1);
+						}
 					}
+					
+					//System.out.print("\n");
+
+					//read.close();
 				}
-				
-				//System.out.print("\n");
 
-				//read.close();
 			}
 			
 		}
