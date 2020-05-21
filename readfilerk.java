14314023ecceb0ee19d53596db562de690e1202a
shehan82 <shehansandeepa82@.com>
2020-05-21 08:59:47 +0530

    Now the seraching start with line num 22

--- readfilerk.java
+++ readfilerk.java
@@ -32,77 +32,79 @@ class readfilerk
 			{
 				count++;
 				
-				if(count>20)
+
+				String text = read.nextLine();
+
+				if(count>21)
 				{
+									int t_length = text.length();
+				char[] t_array = text.toCharArray();
 
-					String text = read.nextLine();
-					int t_length = text.length();
-					char[] t_array = text.toCharArray();
+				char[] p_array = pattern.toCharArray();
+				int p_length = pattern.length();
 
-					char[] p_array = pattern.toCharArray();
-					int p_length = pattern.length();
+				int p_hash = 0;
+		    	int t_hash = 0;
+				int i,j,k,q=101;
+		    	
+		    	int h = 1;
+		    	
+		    	for(i=0;i<p_length-1;i++)
+		    	{
+		    		h=(h*d)%q;
+		    	}
 
-					int p_hash = 0;
-			    	int t_hash = 0;
-					int i,j,k,q=101;
-			    	
-			    	int h = 1;
-			    	
-			    	for(i=0;i<p_length-1;i++)
-			    	{
-			    		h=(h*d)%q;
-			    	}
-
-					if(p_length>t_length)
-					{
-						continue;
-					}
-					//////////////////////////////////////
-					for(j=0;j<p_length;j++)
-			    	{
-			    		
-
-			    		
-			    		p_hash = (d*p_hash + p_array[j])%q;
-			    		t_hash = (d*t_hash + t_array[j])%q;
-			    	}
-
-
-
-			    	for(i=0;i<=t_length-p_length;i++)
-			    	{
-			    		////////////////////////////
-			    		if(t_array[i]==w)
-			    			break;
-
-			    		if(t_hash==p_hash)
-			    		{
-			    			for(j=0;j<p_length;j++)
-			    			{
-			    				if(t_array[i+j]!=p_array[j])
-			    				{
-			    					break;
-			    				}
-			    			}
-
-			    			if(j==p_length)
-			    			{
-			    				System.out.printf("word occurs in line num : %d and column %d \n",count-20,i+1);
-			    			}
-			    		}
-
-			    		if(i<t_length-p_length)
-			    		{
-			    			t_hash = (d*(t_hash-t_array[i]*h)+t_array[i+p_length])%q;
-
-			    			if(t_hash<0)
-			    			{
-			    				t_hash=t_hash+q;
-			    			}
-			    		}
-			    	}
-
+				if(p_length>t_length)
+				{
+					continue;
 				}
+				//////////////////////////////////////
+				for(j=0;j<p_length;j++)
+		    	{
+		    		
+
+		    		
+		    		p_hash = (d*p_hash + p_array[j])%q;
+		    		t_hash = (d*t_hash + t_array[j])%q;
+		    	}
+
+
+
+		    	for(i=0;i<=t_length-p_length;i++)
+		    	{
+		    		////////////////////////////
+		    		if(t_array[i]==w)
+		    			break;
+
+		    		if(t_hash==p_hash)
+		    		{
+		    			for(j=0;j<p_length;j++)
+		    			{
+		    				if(t_array[i+j]!=p_array[j])
+		    				{
+		    					break;
+		    				}
+		    			}
+
+		    			if(j==p_length)
+		    			{
+		    				System.out.printf("word occurs in line num : %d and column %d \n",count,i+1);
+		    			}
+		    		}
+
+		    		if(i<t_length-p_length)
+		    		{
+		    			t_hash = (d*(t_hash-t_array[i]*h)+t_array[i+p_length])%q;
+
+		    			if(t_hash<0)
+		    			{
+		    				t_hash=t_hash+q;
+		    			}
+		    		}
+		    	}
+				}
+
+
 				
 
 				
