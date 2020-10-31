e8bb7f08cf21c8654a3a9f0aa459c49403e49208
shehan82 <shehansandeepa82@gmail.com>
2020-10-31 11:42:20 +0530

    add doPost method

--- eclipse workspace/my/src/com/my/addServlet.java
+++ eclipse workspace/my/src/com/my/addServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class addServlet extends HttpServlet 
 {
-	public void service(HttpServletRequest req, HttpServletResponse res ) throws IOException
+	public void doPost(HttpServletRequest req, HttpServletResponse res ) throws IOException
 	{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
