3748a90565490772142db3f690fc3874d7d25514
shehan82 <shehansandeepa82@gmail.com>
2020-11-01 20:51:03 +0530

    create studetDAO class and student class

--- eclipse workspace/my/src/com/my/AddtoDB.java
+++ eclipse workspace/my/src/com/my/AddtoDB.java
@@ -9,23 +9,45 @@ public class AddtoDB extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 	{
-		try 
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/forJava","root","");
-			Statement st = con.createStatement();
-		}
-		
-		catch (ClassNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		studentDAO dao = new studentDAO();
 	}
 
+}
+
+class studentDAO
+{
+	Connection con = null;
+	
+	public void connect() throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/forJava","root","");
+	}
+	
+	
+	
+	public student addStudent(student s) throws Exception
+	{
+		String query = "insert into student values (?,?,?,?)";
+		PreparedStatement pst = con.prepareStatement(query);
+		pst.setInt(1, s.id);
+		pst.setString(2, s.name);
+		pst.setString(3, s.school);
+		pst.setString(4, s.grade);
+		
+		pst.executeUpdate();
+		
+		
+		return s;
+		
+		
+	}
+}
+
+class student
+{
+		int id;
+		String name, school, grade;
+		
 }
 
