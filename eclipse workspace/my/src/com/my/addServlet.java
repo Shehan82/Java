db38f7e3b3cc4f6fff3fc1b4f379e694d67ab818
shehan82 <shehansandeepa82@gmail.com>
2020-11-09 19:04:12 +0530

    servlet refactor

--- eclipse workspace/my/src/com/my/addServlet.java
+++ eclipse workspace/my/src/com/my/addServlet.java
@@ -1,31 +1,55 @@
 package com.my;
 
 import java.io.IOException;
+import java.sql.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+
 
 public class addServlet extends HttpServlet 
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse res ) throws IOException, ServletException
 	{
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
 		
-		int k = i+j;
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/salon","root","");
+			
+			Statement st = con.createStatement();
+			
+			ResultSet rs = st.executeQuery("select * from managers");
+			
+			rs.next();
+
+			String name = rs.getString("nic");
+			
+//			String json = new Gson().toJson(name);
+//			res.setContentType("application/json");
+//	         res.setCharacterEncoding("UTF-8");
+			res.getWriter().write(name);
+			
+	         
+			
+			
+		} 
 		
-////		res.getWriter().println(k);
-//		//pass k to the sq
-//		req.setAttribute("k", k);
-//		
-//		//call to the sq
-//		RequestDispatcher rd = req.getRequestDispatcher("sq");
-//		rd.forward(req, res);
+		catch (ClassNotFoundException | SQLException e) 
+		{
+			e.printStackTrace();
+		}
 		
-		res.sendRedirect("sq?k="+k);
+		
+		
+	
+		
+		
+		
 		 
 	}
 	
