8bc811d643d25734baf32b42c49597665aa3390f
shehan82 <shehansandeepa82@gmail.com>
2020-11-09 23:54:19 +0530

    fetch data from database and turn into the json object

--- eclipse workspace/my/src/com/my/addServlet.java
+++ eclipse workspace/my/src/com/my/addServlet.java
@@ -2,13 +2,19 @@ package com.my;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.google.gson.Gson;
+
+
+
+
 
 
 public class addServlet extends HttpServlet 
@@ -18,21 +24,13 @@ public class addServlet extends HttpServlet
 		
 		try 
 		{
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/salon","root","");
-			
-			Statement st = con.createStatement();
-			
-			ResultSet rs = st.executeQuery("select * from managers");
-			
-			rs.next();
-
-			String name = rs.getString("nic");
-			
-//			String json = new Gson().toJson(name);
-//			res.setContentType("application/json");
-//	         res.setCharacterEncoding("UTF-8");
-			res.getWriter().write(name);
+			ManagerModel manager = new ManagerModel();
+		ArrayList<ManagerDetails> managerDetails =	manager.getManagersDetails();
+////			
+			String json = new Gson().toJson(managerDetails);
+			res.setContentType("application/json");
+	        res.setCharacterEncoding("UTF-8");
+			res.getWriter().write(json);
 			
 	         
 			
@@ -41,6 +39,9 @@ public class addServlet extends HttpServlet
 		
 		catch (ClassNotFoundException | SQLException e) 
 		{
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
